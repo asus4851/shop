f7c7f7053849301caa8d0e42199468fb7758bcc3
asus4851 <hres.vadym@gmail.com>
2015-12-23 22:50:18 +0200

    added pretty url and fixed sign in

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,28 +28,31 @@ AppAsset::register($this);
     <?php
     NavBar::begin([
         'brandLabel' => 'My Company',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
+        'brandUrl'   => Yii::$app->homeUrl,
+        'options'    => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    $navItems=[
+    $navItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
         ['label' => 'Status', 'url' => ['/status/index']],
         ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']]
+        ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
-    if (Yii::$app->user->isGuest) {
-        array_push($navItems,['label' => 'Sign In', 'url' => ['/user/login']],['label' => 'Sign Up', 'url' => ['/user/register']]);
-    } else {
-        array_push($navItems,['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                              'url' => ['/site/logout'],
-                              'linkOptions' => ['data-method' => 'post']]
+    if( Yii::$app->user->isGuest )
+    {
+        array_push($navItems, ['label' => 'Sign In', 'url' => ['/user/login']], ['label' => 'Sign Up',
+                                                                                 'url'   => ['/user/register']]);
+    } else
+    {
+        array_push($navItems, ['label'       => 'Logout (' . Yii::$app->user->identity->username . ')',
+                               'url'         => ['/site/logout'],
+                               'linkOptions' => ['data-method' => 'post']]
         );
     }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $navItems,
+        'items'   => $navItems,
     ]);
     NavBar::end();
     ?>
